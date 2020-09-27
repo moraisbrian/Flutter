afa09cf0654240c584adb8eef6a2c3c1e9ae5c8d
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-09-27 09:04:32 -0300

    Aula 116

---
+++ chat/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MyApp());
+  await Firebase.initializeApp();
+  FirebaseFirestore.instance
+      .collection("col")
+      .doc("doc")
+      .set({"Nome": "Brian"});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
