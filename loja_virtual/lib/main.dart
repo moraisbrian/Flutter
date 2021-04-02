bb32d2d0d2ccefbbcca88f99c477f04e660e07e5
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-04-02 15:51:37 -0300

    Aula 159

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/home_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -25,14 +26,17 @@ class MyApp extends StatelessWidget {
           default:
             return ScopedModel<UserModel>(
               model: UserModel(),
-              child: MaterialApp(
-                title: 'Loja Virtual',
-                theme: ThemeData(
-                  primarySwatch: Colors.blue,
-                  primaryColor: Color.fromARGB(255, 4, 125, 141),
+              child: ScopedModel<CartModel>(
+                model: CartModel(),
+                child: MaterialApp(
+                  title: 'Loja Virtual',
+                  theme: ThemeData(
+                    primarySwatch: Colors.blue,
+                    primaryColor: Color.fromARGB(255, 4, 125, 141),
+                  ),
+                  debugShowCheckedModeBanner: false,
+                  home: HomeScreen(),
                 ),
-                debugShowCheckedModeBanner: false,
-                home: HomeScreen(),
               ),
             );
         }
