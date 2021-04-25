349736152d782c4eed17b18cc8c41dc90c2dbd0a
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-04-25 09:58:53 -0300

    Aula 214

---
+++ animation/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:animation/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
---
+++ animation/lib/screens/login/login_screen.dart
@@ -0,0 +1,48 @@
+import 'package:animation/screens/login/widgets/form_container.dart';
+import 'package:animation/screens/login/widgets/sign_up_button.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 70, bottom: 32),
+                      child: Image.asset(
+                        'images/check_icon.png',
+                        width: 150,
+                        height: 150,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    FormContainer(),
+                    SignUpButton(),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
