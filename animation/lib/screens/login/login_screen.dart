93109f13b42e19a7af3e86ad404e78c8e59bd922
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-04-25 15:21:18 -0300

    Aula 216

--- animation/lib/screens/login/login_screen.dart
+++ animation/lib/screens/login/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:animation/screens/login/widgets/form_container.dart';
 import 'package:animation/screens/login/widgets/sign_up_button.dart';
 import 'package:animation/screens/login/widgets/stagger_animation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -29,6 +30,7 @@ class _LoginScreenState extends State<LoginScreen>
 
   @override
   Widget build(BuildContext context) {
+    timeDilation = 1;
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
--- animation/lib/screens/login/widgets/stagger_animation.dart
+++ animation/lib/screens/login/widgets/stagger_animation.dart
@@ -3,32 +3,57 @@ import 'package:flutter/material.dart';
 class StaggerAnimation extends StatelessWidget {
   final AnimationController controller;
 
-  StaggerAnimation({this.controller});
+  StaggerAnimation({this.controller})
+      : buttonSqueeze = Tween(
+          begin: 320.0,
+          end: 60.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.0, 0.150),
+          ),
+        );
+
+  final Animation<double> buttonSqueeze;
 
   Widget _buildAnimation(context, child) {
     return Padding(
       padding: EdgeInsets.only(bottom: 50),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          controller.forward();
+        },
         child: Container(
-          width: 360,
+          width: buttonSqueeze.value,
           height: 60,
           alignment: Alignment.center,
           decoration: BoxDecoration(
             color: Color.fromRGBO(247, 64, 106, 1.0),
             borderRadius: BorderRadius.all(Radius.circular(30.0)),
           ),
-          child: Text(
-            'Sing in',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.w300,
-                letterSpacing: 0.3),
-          ),
+          child: _buildInside(context),
         ),
       ),
     );
+  }
+
+  Widget _buildInside(context) {
+    if (buttonSqueeze.value > 75) {
+      return Text(
+        'Sing in',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.3,
+        ),
+      );
+    } else {
+      return CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        strokeWidth: 1.0,
+      );
+    }
   }
 
   @override
